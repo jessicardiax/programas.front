0a9d59da291f9bd69bdee0f01724cade5b562603
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2023-03-07 08:30:13 -0300

    Add files via upload

---
+++ javas.cript.jsf.js
@@ -0,0 +1,21 @@
+// const primeiroNome = prompt("Digite seu primeiro nome:");
+// const sobrenome= prompt("digite seu sobrenome:");
+
+// const idade = prompt("digite sua idade:");
+
+// const id = 414655;
+
+// console.log(`Ola,${primeiroNome}${sobrenome},voce tem ${idade} e seu ID e ${id}`)
+// alert(`Ola, ${primeiroNome}${sobrenome},voce tem ${idade} e seu id e ${id}`)
+
+let idade ="20";
+
+let novIdade = parseInt(idade) //20
+
+let novoNome = ParseInt(nome)
+
+let salario = 1000
+let novoSalario= salario+ ""
+
+console.log(typeof(novoSalario));
+
